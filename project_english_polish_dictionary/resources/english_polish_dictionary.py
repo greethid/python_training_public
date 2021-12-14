b5b9dc93b5b08f512e1e306189aa4e13f40cefcf
greethid <grzegorzszymikk@gmail.com>
2021-12-14 17:07:24 +0100

    Adding words to dictionary, added 'a'

--- project_english_polish_dictionary/resources/english_polish_dictionary.py
+++ project_english_polish_dictionary/resources/english_polish_dictionary.py
@@ -2,8 +2,17 @@
 First three keys (translation, type, pronunciation) are obligatory
 """
 
-english_polish_dictionary = {'abbreviation': {'translation': 'skrót, cos', 'type': 'noun', 'pronunciation': '[əˌbri:viˈeɪʃən]',
-                                              'additional': 'skrót (wyrazu lub wyrażenia) \nplural: abbreviations' }}
+english_polish_dictionary = {
+    'a': {'translation': 'jakiś, niejaki, jeden, na, w, za',
+          'type': 'PRZEDIM [przed rzecz. policz.]',
+          'pronunciation': '[eɪ] akcentowane, [ə] nieakcentowane',
+          'additional': 'jakiś [najczęściej nie tłumaczone na j. polski], niejaki [przed nazwiskami,imionami],'
+                        ' jeden [czasem pomijane w tłumaczeniu], na [przy określaniu częstotliwości], w [przy określaniu częstotliwości],'
+                        ' za [przy określaniu cen], A - ocena celująca/bardzo dobra [najwyższa ocena w szkole brytyjskiej]'},
+    'abbreviation': {'translation': 'skrót',
+                     'type': 'RZ', 'pronunciation': '[əˌbri:viˈeɪʃən]',
+                     'additional': 'skrót (wyrazu lub wyrażenia)'},
+                             }
 
 print(english_polish_dictionary['abbreviation']['type'])
 
@@ -12,3 +21,8 @@ for key, value in english_polish_dictionary.items():
     print(value)
     for value in english_polish_dictionary[key].values():
         print(value)
+
+print(english_polish_dictionary['a']['additional'])
+tab = english_polish_dictionary['a']['additional'].split(', ')
+for info in tab:
+    print(info)
