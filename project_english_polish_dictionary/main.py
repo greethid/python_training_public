dc2e8ca9ecf52e2da0196cd223a80ab2de3486b7
greethid <grzegorzszymikk@gmail.com>
2021-12-20 17:44:45 +0100

    Writing get_random_word function.

--- project_english_polish_dictionary/main.py
+++ project_english_polish_dictionary/main.py
@@ -1,13 +1,30 @@
 from resources.english_polish_dictionary import eng_pol_dict
+import random
+d = {'VENEZUELA':'CARACAS', 'CANADA':'OTTAWA'}
+random.choice(list(d.values()))
 
 
+def print_hi():
+    print(copy_eng_pol_dict['Aborigine'])
 
-def print_hi(name):
-    pass
+
+def get_random_word():
+    random_word = random.choice(list(eng_pol_dict.keys()))
+    random_value = eng_pol_dict.pop(random_word)
+    print(random_word)
+    print(random_value)
+
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    print(len(eng_pol_dict))
+    copy_eng_pol_dict = eng_pol_dict.copy()
+    word = eng_pol_dict.pop('Aborigine')
+    print(word)
+    print(len(eng_pol_dict))
+    print(len(copy_eng_pol_dict))
+    print_hi()
+    get_random_word()
+    print(len(eng_pol_dict))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
