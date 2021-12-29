05df5578e33a80458a612c8c844398a04d9f4aee
greethid <grzegorzszymikk@gmail.com>
2021-12-29 17:36:47 +0100

    Change main and ask_for_answer functions - add loops and new global variables. Add display_statistics, stop_game and reset_game functions.

--- project_english_polish_dictionary/main.py
+++ project_english_polish_dictionary/main.py
@@ -1,20 +1,27 @@
 """Main module for english-polish dictionary project"""
 
 
-from resources.english_polish_dictionary import eng_pol_dict #to delete in future?
 import resources.functions as f
 
 
 if __name__ == '__main__':
-    print(len(eng_pol_dict))
-    copy_eng_pol_dict = eng_pol_dict.copy()
-    word = eng_pol_dict.pop('Aborigine')
-    print(word)
-    print(len(eng_pol_dict))
-    print(len(copy_eng_pol_dict))
-    f.get_random_word()
-    print(len(eng_pol_dict))
-    # f.start_game()
-    f.ask_for_answer()
+    f.start_game()
+
+    while True:
+        answer = f.ask_for_answer()
+        if answer == 'q':
+            break
+
+    f.stop_game()
+
+
+
+
+
+
+
+
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- project_english_polish_dictionary/resources/functions.py
+++ project_english_polish_dictionary/resources/functions.py
@@ -8,7 +8,8 @@ import random
 
 #Variables
 username = ''
-
+correct_answers = 0
+all_answers = 0
 
 def get_random_word():
     """Pick and return random word from dictionary"""
@@ -22,26 +23,64 @@ def start_game():
     print('Welcome to the game for learning English!')
     username = input('Please type your username and press enter: ')
     print(f'Hello {username.title()}!')
-    print('Write polish translation for word which you see on the screen and press enter. Press "q" to quit.')
+    print('Write polish translation for word which you see on the screen and press enter.'
+          '\nPress "q" to quit.'
+          '\nPress "s" for statistics.'
+          '\nPress "r" for reset game.')
 
 
 def ask_for_answer():
     """Ask user to give correct translation for word and delete that used word from dictionary"""
+    global all_answers
+    global correct_answers
     i = 0
     word = get_random_word()
     length = len(eng_pol_dict[word])
     answer = input(f'{word}: ')
+
+    if answer == 'q':
+        return answer
+
+    while answer == 's' or answer == 'r':
+        if answer == 's':
+            display_statistics()
+        elif answer == 'r':
+            reset_game()
+        elif answer == 'q':
+            return answer
+
+        answer = input(f'{word}: ')
+
     if answer in eng_pol_dict[word]:
         print(BColors.OKGREEN + 'OK' + BColors.ENDC)
+        correct_answers += 1
     else:
         print(BColors.FAIL + 'NOK' + BColors.ENDC)
+
     for translation in eng_pol_dict[word]:
         if i >= length - 1:
             print(translation)
         else:
             print(translation, end=', ')
             i += 1
+
     # delete used word from dictionary
     del eng_pol_dict[word]
+    all_answers += 1
 
 
+def display_statistics():
+    """Display statistics of current game on the screen"""
+    correct_percent = 0
+    if all_answers > 0:
+        correct_percent = correct_answers/all_answers * 100
+    print(f'Correct answers: {correct_answers} out of {all_answers} which is {correct_percent:.0f} percent')
+
+
+def stop_game():
+    """Set of methods for game ending"""
+    display_statistics()
+
+
+def reset_game():
+    pass
