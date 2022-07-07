ae6fa461545d80a43f0170657875cc222543a2d0
greethid <grzeeechooo@gmail.com>
2022-07-07 17:09:11 +0200

    Add class Settings

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -12,6 +12,9 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
+        #Define the background color
+        self.bg_color = (250, 230, 230)
+
     def run_game(self):
         "Start main loop of the program"
 
@@ -20,6 +23,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            #Refresh screen after each iteration of the loop
+            self.screen.fill(self.bg_color)
 
             #Displaying the last modified screen
             pygame.display.flip()
---
+++ alien_invasion/settings.py
@@ -0,0 +1,6 @@
+class Settings:
+    """Class dedicated to keep all game settings"""
+
+    def __init__(self):
+        """Initializing game settings"""
+        #Display settings
