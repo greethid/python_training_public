44e344029972d321777709ed76a27bfa33d689e0
greethid <grzeeechooo@gmail.com>
2022-08-26 16:39:16 +0200

    Add _check_keydown_events and _check_keyup events methods

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -35,17 +35,24 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    #Moving the ship to the right side
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
-
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Reaction to pressing a key"""
+        if event.key == pygame.K_RIGHT:
+            # Moving the ship to the right side
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        """Reaction to releasing a key"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """updating the images on the screen and going to a new screen"""
