ab480c783a8b523fe1525526b4947b6e65acb8da
greethid <grzeeechooo@gmail.com>
2023-02-02 17:02:36 +0100

    Continue writing class Scoreboard

--- alien_invasion/scoreboard.py
+++ alien_invasion/scoreboard.py
@@ -15,6 +15,9 @@ class Scoreboard:
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)
 
+        self.score_image = None
+        self.score_rect = None
+
         # Prepare init scoring images
         self.prep_score()
 
@@ -24,5 +27,11 @@ class Scoreboard:
         self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
 
         # Display scoring in the upper right corner of the screen
-        self.score_rect = self.score_image
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
+
+    def show_score(self):
+        """Displaying the score on the screen"""
+        self.screen.blit(self.score_image, self.score_rect)
 
