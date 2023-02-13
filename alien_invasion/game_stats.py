f154809d9085f47efc3a5aa0b3fb53e1a738bd2b
greethid <grzeeechooo@gmail.com>
2023-02-13 16:59:07 +0100

    Implement the best score

--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -9,6 +9,9 @@ class GameStats:
         # Run game 'alien invasion' in inactive state
         self.game_active = False
 
+        # The best score shall never be reset
+        self.high_score = 0
+
     def reset_stats(self):
         """Initialization of statistical data which can change during play"""
         self.ship_left = self.settings.ship_limit
--- alien_invasion/scoreboard.py
+++ alien_invasion/scoreboard.py
@@ -17,12 +17,15 @@ class Scoreboard:
 
         self.score_image = None
         self.score_rect = None
+        self.high_score_image = None
+        self.high_score_rect = None
 
         # Prepare init scoring images
         self.prep_score()
+        self.prep_high_score()
 
     def prep_score(self):
-        """Transform scores to image on the screen"""
+        """Transform score to image on the screen"""
         rounded_score = round(self.stats.score, -1)
         score_str = '{:,}'.format(rounded_score)
         self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
@@ -32,7 +35,25 @@ class Scoreboard:
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_high_score(self):
+        """Transform the best score to image on the screen"""
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = '{:,}'.format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
+
+        # Display the best score at the top center of the screen
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.screen_rect.top
+
     def show_score(self):
         """Displaying the score on the screen"""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+
+    def check_high_score(self):
+        """Checking if a score is the best so far"""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
 
