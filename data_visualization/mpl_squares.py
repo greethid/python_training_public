757366b85badffa90a30c144cd5b852f0183f92e
greethid <grzeeechooo@gmail.com>
2023-03-28 17:40:41 +0200

    Data visualization - continuation, add new class for random walk

--- data_visualization/mpl_squares.py
+++ data_visualization/mpl_squares.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**3 for x in x_values]
 squares2 = [x**2 for x in range(-100, 100)]
 
+plt.style.use('ggplot')
 fig, ax = plt.subplots()
-ax.plot(input_values, squares, linewidth=3)
+# ax.scatter(x_values, y_values, c=(0, 0.8, 0), s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
+# ax.plot(x_values, y_values, linewidth=3)
 
 # Defining the chart title and axis labels
 ax.set_title('Square of numbers', fontsize=24)
@@ -15,7 +18,13 @@ ax.set_ylabel('Squared values', fontsize=14)
 # Defining the size of the labels
 ax.tick_params(axis='both', labelsize=14)
 
+# Defining the range for each axis
+ax.axis([0, 1100, 0, 1100000])
+
 plt.show()
+
+# Save to a file
+# plt.savefig('squares_plot.png', bbox_inches='tight')
 
 # fig, ax = plt.subplots()
 # ax.plot(squares2)
