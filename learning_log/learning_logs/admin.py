9c9637d1cbd5e7e446a0d81585202f061bbee48b
Szymik <Grzegorz.Szymik@aptiv.com>
2025-07-08 17:53:04 +0200

    Add new model Entry

--- learning_log/learning_logs/admin.py
+++ learning_log/learning_logs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Topic
+from .models import Entry
+
+admin.site.register(Topic)
+admin.site.register(Entry)
+
 # Register your models here.
--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -9,3 +9,16 @@ class Topic(models.Model):
         """Returns a representation of a model as a text string"""
         return self.text
 
+class Entry(models.Model):
+    """Specific information about a progress in learning"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Defines the special attribute that tells Django to use 'entries' form instead of 'Entrys' """
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Returns a representation of a model as a text string - up to 50 characters"""
+        return f"{self.text[:50]}..."
