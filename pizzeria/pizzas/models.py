5e995498db8b50a7b56843abaf95a61362d1b3bd
Szymik <Grzegorz.Szymik@aptiv.com>
2025-07-14 13:45:37 +0200

    Add models Pizza and Toppings

--- pizzeria/pizzas/models.py
+++ pizzeria/pizzas/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Pizza(models.Model):
+    """Name of pizza"""
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        """Returns a representation of a model as a text string"""
+        return self.name
+
+class Topping(models.Model):
+    """Specific information about a progress in learning"""
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    # class Meta:
+    #     """Defines the special attribute that tells Django to use 'entries' form instead of 'Entrys' """
+    #     verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Returns a representation of a model as a text string - up to 50 characters"""
+        if len(self.name) > 50:
+            return f"{self.name[:50]}..."
+        return f"{self.name}"
