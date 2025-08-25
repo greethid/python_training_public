f15e0b517b75c747d8053eee6d596b70249c6204
Szymik <Grzegorz.Szymik@aptiv.com>
2025-08-25 15:53:12 +0200

    Add forms.py

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
     # A detailed page about a single topic
     path('topics/(<int:topic_id>)', views.topic, name='topic'),
+    # A page designed for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic')
 ]
