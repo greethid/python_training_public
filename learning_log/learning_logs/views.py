1a8d6e53986ceb0f03a8bae83a29964c406bb04d
Szymik <Grzegorz.Szymik@aptiv.com>
2025-08-27 15:06:26 +0200

    Add new_topic.html, link to a new topic from topics.html and new_topic function in views.py

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from lib2to3.fixes.fix_input import context
+
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 
@@ -20,3 +23,19 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Add a new topic"""
+    if request.method != 'POST':
+        # no data was submitted, create a new form
+        form = TopicForm()
+    else:
+        # data was passed via a POST request, it needs to be processed
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # Display the empty form
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
