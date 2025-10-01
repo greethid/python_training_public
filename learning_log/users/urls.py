a5da15db4c0d36b50861ac7a94abe52456bafcf2
Szymik <Grzegorz.Szymik@aptiv.com>
2025-10-01 13:24:08 +0200

    Add register page for new users

--- learning_log/users/urls.py
+++ learning_log/users/urls.py
@@ -1,10 +1,13 @@
 """Defines url patterns for users app"""
 
 from django.urls import path, include
+from . import views
 
 app_name = 'users'
 urlpatterns = [
     # Including default authentication urls
     path('', include('django.contrib.auth.urls')),
+    # Registration page
+    path('register/', views.register, name='register')
 ]
 
--- learning_log/users/views.py
+++ learning_log/users/views.py
@@ -1,1 +1,25 @@
-from django.shortcuts import render
+from lib2to3.fixes.fix_input import context
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
+
+def register(request):
+    """Registration of a new user"""
+    if request.method != 'POST':
+        # Display default empty registration form
+        form = UserCreationForm
+    else:
+        # Processing of a completed form
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Logging user and redirection on the main page
+            login(request, new_user)
+            return redirect('learning_logs:index')
+
+    # Display an empty form
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
