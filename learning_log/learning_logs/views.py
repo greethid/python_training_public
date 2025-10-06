d5cbac6c613d9352010375921e5bdcf3847f6e26
Szymik <Grzegorz.Szymik@aptiv.com>
2025-10-06 18:59:59 +0200

    Add login_required decorator, now only registered users can view topic and entries, assign all entries to superuser and migrate database

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,6 +1,5 @@
-from lib2to3.fixes.fix_input import context
-
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -11,12 +10,14 @@ def index(request):
     """Home page for the learning log app"""
     return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
     """Display all topics"""
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """Display a single topic and all related posts"""
     topic = Topic.objects.get(id=topic_id)
@@ -24,6 +25,7 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """Add a new topic"""
     if request.method != 'POST':
@@ -40,6 +42,7 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a specific topic"""
     topic = Topic.objects.get(id=topic_id)
@@ -60,6 +63,7 @@ def new_entry(request, topic_id):
     context = {'topic': topic,'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry"""
     entry = Entry.objects.get(id=entry_id)
